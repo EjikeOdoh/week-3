11d8c2a48ffb3073d5ac5d13033d65b64c3059ea
Ejike Odoh <ejike.developer@gmail.com>
2026-04-27 10:52:06 +0100

    Learn continue, break for loops, scope

---
+++ js/script.js
@@ -0,0 +1,74 @@
+//Loops - ways we can do repetitive actions, based on a condition
+
+// while (condition) {
+//     // commands
+// }
+
+// do {
+//     // command
+// } while (condition)
+
+
+// for (let i = 0; i < 10; i++) {
+//     // Commands
+// }
+
+
+// Challenge - Print numbers 1 to 10, and omit numbers divisible by 3
+// let counter = 1
+
+// while (counter <= 10) {
+//     if (counter % 3 === 0) {
+
+//     } else {
+//         console.log(counter)
+//     }
+//     counter++
+// }
+
+// Is there another way to do this?
+
+// console.log("Start of the loop")
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 3 === 0){
+//         continue
+//     }
+//     console.log(i)
+
+//     if ( i === 7) {
+//         console.log('This is the end')
+//         break;
+//     }
+// }
+
+// console.log("End of the loop statement")
+
+
+// let counter = 0
+
+// while (counter < 10) {
+//      counter++
+//     if (counter % 3 === 0) {
+//         continue
+//     }
+//     console.log(counter)
+// }
+
+
+// let myName = "Deborah"
+// let myAge = 70
+
+// function myFunction () {
+//     let myAge = 10
+//     console.log(myName)
+//     console.log(myAge)
+// }
+
+// console.log(myName)
+// console.log(myAge)
+
+// myFuncti
+
+
+
